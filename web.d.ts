a8d35f8315fed7017aa115e0921a72fba242b6f7
zerkalica <zerkalica@users.noreply.github.com>
2020-06-01 07:02:31 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1176,7 +1176,7 @@ declare namespace $ {
         reset(): void;
         version(): string;
         abstract ensure(next?: boolean): boolean;
-        abstract watcher(): {
+        watcher(): {
             destructor(): void;
         };
         exists(next?: boolean, force?: $mol_mem_force): boolean;
@@ -1233,9 +1233,6 @@ declare namespace $ {
         static relative(path: string): $mol_file_web;
         static base: string;
         buffer(next?: Uint8Array, force?: $mol_mem_force): Uint8Array;
-        watcher(): {
-            destructor(): void;
-        };
         stat(next?: $mol_file_stat, force?: $mol_mem_force): $mol_file_stat;
         resolve(path: string): $mol_file_web;
         ensure(next?: boolean): boolean;
