0d1ae036d62b3a815c45d3eeca7cd3ac19e8388c
nin-jin <nin-jin@users.noreply.github.com>
2020-12-04 07:05:44 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2266,6 +2266,7 @@ declare namespace $ {
         value(val?: any): any;
         hint(): string;
         enabled(): boolean;
+        length_max(): number;
         Edit(): $$.$mol_string;
         View(): $$.$mol_text_code;
     }
