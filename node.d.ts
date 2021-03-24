649dae102d45858b4aaa2b3efaf2d58897e529e4
nin-jin <nin-jin@users.noreply.github.com>
2021-03-24 09:25:55 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -122,12 +122,10 @@ declare namespace $ {
     let $mol_log3_stack: (() => void)[];
 }
 
-/// <reference types="node" />
 interface $node {
     [key: string]: any;
 }
 declare var $node: $node;
-declare const $node_require: NodeRequire;
 
 declare namespace $ {
     function $mol_deprecated(message: string): <Method extends (this: Host, ...args: readonly any[]) => any, Host extends { [key in Field]: Method; } & {
