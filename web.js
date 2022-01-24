4df122e9a560b8f31f4d6da483f25d9feba91e5f
nin-jin <nin-jin@users.noreply.github.com>
2022-01-24 07:21:36 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -2833,6 +2833,9 @@ var $;
 var $;
 (function ($) {
     class $mol_page extends $mol_view {
+        dom_name() {
+            return "article";
+        }
         sub() {
             return [
                 this.Head(),
@@ -2865,6 +2868,7 @@ var $;
         Head() {
             const obj = new this.$.$mol_view();
             obj.minimal_height = () => 64;
+            obj.dom_name = () => "header";
             obj.sub = () => this.head();
             return obj;
         }
@@ -2887,6 +2891,7 @@ var $;
         }
         Foot() {
             const obj = new this.$.$mol_view();
+            obj.dom_name = () => "footer";
             obj.sub = () => this.foot();
             return obj;
         }
@@ -4278,6 +4283,9 @@ var $;
 var $;
 (function ($) {
     class $mol_paragraph extends $mol_view {
+        dom_name() {
+            return "p";
+        }
         line_height() {
             return 24;
         }
@@ -4296,6 +4304,13 @@ var $;
     $.$mol_paragraph = $mol_paragraph;
 })($ || ($ = {}));
 //mol/paragraph/-view.tree/paragraph.view.tree.ts
+;
+"use strict";
+var $;
+(function ($) {
+    $mol_style_attach("mol/paragraph/paragraph.view.css", "[mol_paragraph] {\n\tmargin: 0;\n}\n");
+})($ || ($ = {}));
+//mol/paragraph/-css/paragraph.view.css.ts
 ;
 "use strict";
 var $;
