f6a957ea5b7979fd1d91c9892d7c5bab91e66200
jin <jin@hyoo.ru>
2022-07-22 12:39:39 +0300

    Less code in sample

--- perf.view.ts
+++ perf.view.ts
@@ -93,14 +93,10 @@ namespace $.$$ {
 			
 			const code = this.prefix()
 				+ '\n' + this.case_prefix( index ) 
-				+ `\n/**/const backup_${token} = $mol_wire_auto()`
-				+ `\n$mol_wire_auto( null )`
 				+ '\n' + this.source( index )
-				+ `\n$mol_wire_auto( backup_${token} )`
-				+ `\nvoid 0`
 				+ '\n' + this.postfix()
 			
-			return code.replace( /\{#\}/g , '1' )
+			return code.trim().replace( /\{#\}/g , '1' )
 		}
 		
 		@ $mol_mem_key
