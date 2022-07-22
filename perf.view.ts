d73b32cd5c83f9ead686a4765d9a4928b7e9839d
jin <jin@hyoo.ru>
2022-07-22 13:45:02 +0300

    No 0 mem

--- perf.view.ts
+++ perf.view.ts
@@ -371,11 +371,13 @@ namespace $.$$ {
 		}
 
 		memory_total() {
-			return $mol_si_short( this.result().memory, 'B' )
+			const val = this.result().memory
+			return val ? $mol_si_short( val, 'B' ) : '?B'
 		}
 
 		memory_per_iteration() {
-			return $mol_si_short( this.result().memory_per_iteration, 'B' )
+			const val = this.result().memory_per_iteration
+			return val ? $mol_si_short( val, 'B' ) : '?B'
 		}
 
 		frequency_portion() {
