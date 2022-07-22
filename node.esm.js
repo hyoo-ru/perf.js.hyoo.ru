ce7571c5a644d6da1b7ecf6a35031e8c5bbb90c4
nin-jin <nin-jin@users.noreply.github.com>
2022-07-22 08:39:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -9037,9 +9037,9 @@ var $;
                 if (typeof value === 'function') {
                     const name = Reflect.getOwnPropertyDescriptor(value, 'name')?.value;
                     const source = Function.prototype.toString.call(value);
-                    const args = source.replace(/\)[\s\S]*$/g, ')').replace(/^[\s\S]*\(/g, '(');
+                    const args = source.match(/^[^{]+(\([\s\S]*?\))/)?.[1] ?? '{}';
                     if (name)
-                        return name + args + '{}';
+                        return name + args;
                 }
                 if (value instanceof RegExp)
                     return String(value);
@@ -9075,7 +9075,7 @@ var $;
                 }
                 if (value instanceof Function) {
                     let source = Function.prototype.toString.call(value)
-                        .replace(/^.*?\{\r?\n/, '')
+                        .replace(/^.*?\{\r?\n?/, '')
                         .replace(/}$/, '')
                         .trimEnd();
                     const indent = source.match(/^\s*/)[0];
@@ -9578,7 +9578,7 @@ var $;
             }
             code_enhanced() {
                 let code = this.code();
-                code = code.replaceAll(/^([ \t]*)(?:const|var|let) +(\w+)/mig, (found, indent, name) => `spy( ()=>[ "${indent}${name} =", ${name} ] );${found}`);
+                code = code.replaceAll(/^([ \t]*)(?:const|var|let|class|function) +(\w+)/mig, (found, indent, name) => `spy( ()=>[ "${indent}${name} =", ${name} ] );${found}`);
                 return code;
             }
             execute() {
