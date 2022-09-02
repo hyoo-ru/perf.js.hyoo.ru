d0220ae4d2addb04998c8bad6f584feb7c9a6626
nin-jin <nin-jin@users.noreply.github.com>
2022-09-02 07:29:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -1008,7 +1008,7 @@ var $;
                         return res;
                     };
                     result = Object.assign(result.then(put, put), {
-                        destructor: result['destructor']
+                        destructor: result['destructor'] ?? (() => { })
                     });
                     handled.add(result);
                 }
@@ -1025,7 +1025,7 @@ var $;
                         if (this.cache === result)
                             this.absorb();
                     }), {
-                        destructor: result['destructor']
+                        destructor: result['destructor'] ?? (() => { })
                     });
                     handled.add(result);
                 }
@@ -1148,6 +1148,8 @@ var $;
             };
         }
         complete() {
+            if (this.cache instanceof Promise)
+                return;
             this.destructor();
         }
         put(next) {
@@ -1275,7 +1277,9 @@ var $;
                     try {
                         next[Symbol.toStringTag] = this[Symbol.toStringTag];
                     }
-                    catch { }
+                    catch {
+                        Object.defineProperty(next, Symbol.toStringTag, { value: this[Symbol.toStringTag] });
+                    }
                 }
                 if (this.sub_from < this.data.length) {
                     if (!$mol_compare_deep(prev, next)) {
@@ -2266,7 +2270,7 @@ var $;
         }
         dom_tree(next) {
             const node = this.dom_node(next);
-            try {
+            render: try {
                 $mol_dom_render_attributes(node, { mol_view_error: null });
                 try {
                     this.render();
@@ -2278,21 +2282,26 @@ var $;
                         }
                     }
                 }
-                this.auto();
             }
             catch (error) {
                 $mol_fail_log(error);
                 $mol_dom_render_attributes(node, { mol_view_error: error.name || error.constructor.name });
                 if (error instanceof Promise)
-                    return node;
+                    break render;
                 if ((error_showed.get(error) ?? this) !== this)
-                    return node;
+                    break render;
                 try {
                     const message = error.message || error;
                     node.innerText = message.replace(/^|$/mg, '\xA0\xA0');
                 }
                 catch { }
                 error_showed.set(error, this);
+            }
+            try {
+                this.auto();
+            }
+            catch (error) {
+                $mol_fail_log(error);
             }
             return node;
         }
