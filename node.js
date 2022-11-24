5a7d1be8faef7fa0d3a3a7738a20f6e6d34f0d25
nin-jin <nin-jin@users.noreply.github.com>
2022-11-24 07:20:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -7977,7 +7977,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
--- node.test.js
+++ node.test.js
@@ -7969,7 +7969,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
