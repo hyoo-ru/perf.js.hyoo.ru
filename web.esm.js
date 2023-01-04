904e3c0b71ab17c97ca2aea076d9e9c02324ab42
nin-jin <nin-jin@users.noreply.github.com>
2023-01-04 07:18:42 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -11403,6 +11403,9 @@ var $;
             try {
                 this.watch()?.();
             }
+            catch (error) {
+                $mol_fail_log(error);
+            }
             finally {
                 for (const pub of $mol_wire_auto().pub_list) {
                     if (pub instanceof $mol_wire_fiber) {
