aa92ac8a0fd403a664bae6b50d13e2ea9915733a
nin-jin <nin-jin@users.noreply.github.com>
2023-01-14 15:46:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -1824,7 +1824,6 @@ var $;
                 if ($mol_owning_check(this, prev)) {
                     prev.destructor();
                 }
-                this.cache = next;
                 if ($mol_owning_catch(this, next)) {
                     try {
                         next[Symbol.toStringTag] = this[Symbol.toStringTag];
@@ -1836,6 +1835,7 @@ var $;
                 if (!this.sub_empty)
                     this.emit();
             }
+            this.cache = next;
             this.cursor = $mol_wire_cursor.fresh;
             if (next instanceof Promise)
                 return next;
--- node.test.js
+++ node.test.js
@@ -1816,7 +1816,6 @@ var $;
                 if ($mol_owning_check(this, prev)) {
                     prev.destructor();
                 }
-                this.cache = next;
                 if ($mol_owning_catch(this, next)) {
                     try {
                         next[Symbol.toStringTag] = this[Symbol.toStringTag];
@@ -1828,6 +1827,7 @@ var $;
                 if (!this.sub_empty)
                     this.emit();
             }
+            this.cache = next;
             this.cursor = $mol_wire_cursor.fresh;
             if (next instanceof Promise)
                 return next;
