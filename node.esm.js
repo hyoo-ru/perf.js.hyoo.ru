2908a93b226f56f8c1c95b9c91a5ae6a98800c2f
nin-jin <nin-jin@users.noreply.github.com>
2023-02-25 07:19:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -9497,6 +9497,8 @@ var $;
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 el.value = this.value_changed(el.value);
+                if (to === null)
+                    return;
                 el.selectionEnd = to;
                 el.selectionStart = from;
                 this.selection_change(next);
@@ -9525,9 +9527,15 @@ var $;
                 el.selectionStart = from;
             }
             selection_start() {
+                const el = this.dom_node();
+                if (el.selectionStart === null)
+                    return undefined;
                 return this.selection()[0];
             }
             selection_end() {
+                const el = this.dom_node();
+                if (el.selectionEnd === null)
+                    return undefined;
                 return this.selection()[1];
             }
         }
