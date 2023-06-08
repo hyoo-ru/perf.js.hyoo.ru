11b4671b0c4d713ea19cae9bd41b37cc836c31ef
nin-jin <nin-jin@users.noreply.github.com>
2023-06-08 07:17:50 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -2845,7 +2845,12 @@ var $;
         ensure_visible(view, align = "start") {
             const path = this.view_find(v => v === view).next().value;
             this.force_render(new Set(path));
-            this.dom_final();
+            try {
+                this.dom_final();
+            }
+            catch (err) {
+                $mol_fail_log(err);
+            }
             view.dom_node().scrollIntoView({ block: align });
         }
         bring() {
@@ -3378,7 +3383,7 @@ var $;
 //mol/book2/-css/book2.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "3406820";
+let $hyoo_sync_revision = "6ffbb1f";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -5021,7 +5026,7 @@ var $;
         land_init(land) {
             this.db_land_init(land);
             try {
-                this.db_land_sync(land);
+                this.land_sync(land);
             }
             catch (error) {
                 $mol_fail_log(error);
--- node.test.js
+++ node.test.js
@@ -2837,7 +2837,12 @@ var $;
         ensure_visible(view, align = "start") {
             const path = this.view_find(v => v === view).next().value;
             this.force_render(new Set(path));
-            this.dom_final();
+            try {
+                this.dom_final();
+            }
+            catch (err) {
+                $mol_fail_log(err);
+            }
             view.dom_node().scrollIntoView({ block: align });
         }
         bring() {
@@ -3370,7 +3375,7 @@ var $;
 //mol/book2/-css/book2.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "3406820";
+let $hyoo_sync_revision = "6ffbb1f";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -5013,7 +5018,7 @@ var $;
         land_init(land) {
             this.db_land_init(land);
             try {
-                this.db_land_sync(land);
+                this.land_sync(land);
             }
             catch (error) {
                 $mol_fail_log(error);
