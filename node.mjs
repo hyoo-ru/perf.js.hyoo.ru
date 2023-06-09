e33954d6f79f0a57518d04e6f2a69810b90914f6
nin-jin <nin-jin@users.noreply.github.com>
2023-06-09 07:18:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -3383,7 +3383,7 @@ var $;
 //mol/book2/-css/book2.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "6ffbb1f";
+let $hyoo_sync_revision = "6efc2c3";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
