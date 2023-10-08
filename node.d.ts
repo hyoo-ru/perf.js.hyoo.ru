120a9dbdd22d594e2766ffdc40f9372b8c846559
nin-jin <nin-jin@users.noreply.github.com>
2023-10-08 07:16:26 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1458,7 +1458,7 @@ declare namespace $ {
 
 declare namespace $ {
     function $mol_wire_race<Tasks extends ((...args: any) => any)[]>(...tasks: Tasks): {
-        [index in keyof Tasks]: index extends number ? ReturnType<Tasks[index]> : Tasks[index];
+        [index in keyof Tasks]: ReturnType<Tasks[index]>;
     };
 }
 
