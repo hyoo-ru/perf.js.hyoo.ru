3e659ed1fc4d9ffe0a8c4a4b688e548dc981d3aa
nin-jin <nin-jin@users.noreply.github.com>
2023-11-10 07:16:38 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1133,6 +1133,24 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    let $mol_mem_persist: typeof $mol_wire_solid;
+}
+
+declare namespace $ {
+    class $mol_storage extends $mol_object2 {
+        static native(): {
+            estimate: () => StorageEstimate;
+            getDirectory: () => FileSystemDirectoryHandle;
+            persist: () => boolean;
+            persisted: () => boolean;
+        };
+        static persisted(next?: boolean): boolean;
+        static estimate(): StorageEstimate;
+        static dir(): FileSystemDirectoryHandle;
+    }
+}
+
+declare namespace $ {
     class $mol_state_local<Value> extends $mol_object {
         static 'native()': Pick<Storage, 'getItem' | 'setItem' | 'removeItem'>;
         static native(): Storage | {
@@ -3072,6 +3090,7 @@ declare namespace $ {
         event(): Record<string, any>;
         sub(): readonly any[];
         symbols_alt(): Record<string, any>;
+        symbols_alt_ctrl(): Record<string, any>;
         symbols_alt_shift(): Record<string, any>;
         clickable(next?: any): boolean;
         sidebar_showed(): boolean;
