7b026758c313eeaea4b9042f731c94f699e092d2
nin-jin <nin-jin@users.noreply.github.com>
2024-12-08 07:23:23 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@a6aac9c1931dbaf2d892703b924a30469e457011 🚀

--- node.js
+++ node.js
@@ -9749,7 +9749,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
--- web.mjs
+++ web.mjs
@@ -9366,7 +9366,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
