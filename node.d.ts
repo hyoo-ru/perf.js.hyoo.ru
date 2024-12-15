7455b5050bad21fce8b203f738ffc9e1d24b45ea
nin-jin <nin-jin@users.noreply.github.com>
2024-12-15 07:21:56 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@a6aac9c1931dbaf2d892703b924a30469e457011 🚀

--- node.d.ts
+++ node.d.ts
@@ -3149,7 +3149,7 @@ declare namespace $ {
 declare namespace $ {
     class $mol_syntax2<Lexems extends {
         [name: string]: RegExp;
-    }> {
+    } = {}> {
         lexems: Lexems;
         constructor(lexems: Lexems);
         rules: Array<{
