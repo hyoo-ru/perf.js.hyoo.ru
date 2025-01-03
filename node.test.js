367fb7d06315c22e860f0b6c73a21a9333adf6e0
nin-jin <nin-jin@users.noreply.github.com>
2025-01-03 07:21:08 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@a6aac9c1931dbaf2d892703b924a30469e457011 🚀

--- node.test.js
+++ node.test.js
@@ -11327,7 +11327,7 @@ var $;
 			return "async";
 		}
 		cors(){
-			return "anonymous";
+			return null;
 		}
 		natural_width(){
 			return 0;
--- web.mjs
+++ web.mjs
@@ -11011,7 +11011,7 @@ var $;
 			return "async";
 		}
 		cors(){
-			return "anonymous";
+			return null;
 		}
 		natural_width(){
 			return 0;
@@ -13978,8 +13978,9 @@ var $;
                     if (response.status !== 200)
                         return response;
                     const headers = new Headers(response.headers);
-                    headers.set("Cross-Origin-Embedder-Policy", "credentialless");
+                    headers.set("Cross-Origin-Embedder-Policy", "require-corp");
                     headers.set("Cross-Origin-Opener-Policy", "same-origin");
+                    headers.set("$mol_offline", "");
                     response = new Response(response.body, {
                         status: response.status,
                         statusText: response.statusText,
