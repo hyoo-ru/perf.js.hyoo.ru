b211929931048605af083530164ebea2ba14c6aa
nin-jin <nin-jin@users.noreply.github.com>
2025-07-20 07:25:17 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@a6aac9c1931dbaf2d892703b924a30469e457011 🚀

--- node.d.ts
+++ node.d.ts
@@ -91,6 +91,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_guid(length?: number, exists?: (id: string) => boolean): string;
+}
+
+declare namespace $ {
     enum $mol_wire_cursor {
         stale = -1,
         doubt = -2,
@@ -101,6 +105,8 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_wire_pub extends Object {
+        constructor(id?: string);
+        [Symbol.toStringTag]: string;
         data: unknown[];
         static get [Symbol.species](): ArrayConstructor;
         protected sub_from: number;
@@ -212,7 +218,6 @@ declare namespace $ {
         static plan_task: $mol_after_tick | null;
         static plan(): void;
         static sync(): void;
-        [Symbol.toStringTag]: string;
         cache: Result | Error | Promise<Result | Error>;
         get args(): Args;
         result(): Result | undefined;
@@ -237,10 +242,6 @@ declare namespace $ {
         step(): Promise<null>;
         destructor(): void;
     }
-}
-
-declare namespace $ {
-    function $mol_guid(length?: number, exists?: (id: string) => boolean): string;
 }
 
 declare namespace $ {
@@ -5663,6 +5664,11 @@ declare namespace $ {
         clear(): void;
         item(val: Value, next?: boolean): boolean;
     }
+}
+
+declare namespace $ {
+    function $mol_wire_proxy_pub(id: string, target: object): $mol_wire_pub;
+    function $mol_wire_proxy<Target>(id: string, target: Target): Target;
 }
 
 declare namespace $ {
