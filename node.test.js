02c7e0aa20a683a0c644208da78c3d31e2a99749
nin-jin <nin-jin@users.noreply.github.com>
2025-09-09 07:24:35 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@a6aac9c1931dbaf2d892703b924a30469e457011 🚀

--- node.test.js
+++ node.test.js
@@ -11805,6 +11805,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
