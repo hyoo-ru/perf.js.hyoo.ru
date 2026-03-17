8e55a27e08f177a3e99ce92192569cb64f24c7d1
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:58:13 +0000

    Deploying to gh-pages from @ hyoo-ru/perf.js.hyoo.ru@6fa27b864c9e9e4079a8479e7c86776c19a1324b 🚀

--- node.mjs
+++ node.mjs
@@ -8369,6 +8369,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- node.test.js
+++ node.test.js
@@ -8360,6 +8360,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
